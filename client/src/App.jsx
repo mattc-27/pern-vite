cffd6331cadae0b65c9187d486b5782f0553f248
mattc-27 <mattcopeland927@gmail.com>
2023-05-21 12:00:59 -0600

    newUpdates

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Vite template for PERN stack.</h1>
+    </div>
+  )
+}
+
+export default App
